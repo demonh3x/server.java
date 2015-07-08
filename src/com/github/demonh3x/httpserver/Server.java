24feaaf827efb0cc1833fbd4bc2d34c995826c48
Mateu Adsuara <mateuadsuara@gmail.com>
2015-07-08 10:21:20 +0100

    Server uses the specified port

--- src/com/github/demonh3x/httpserver/Server.java
+++ src/com/github/demonh3x/httpserver/Server.java
@@ -1,10 +1,20 @@
 package com.github.demonh3x.httpserver;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Server {
+    private final int port;
+
     public Server(int port) {
+        this.port = port;
     }
 
     public void start() {
-
+        try {
+            new ServerSocket(port);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
