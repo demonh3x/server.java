90831cfdeb068cbc5b8bbf73077e5925d73a458b
Mateu Adsuara <mateuadsuara@gmail.com>
2015-07-09 18:48:52 +0100

    Adds ServeFiles RequestHandler

---
+++ src/com/github/demonh3x/server/http/ServeFiles.java
@@ -0,0 +1,56 @@
+package com.github.demonh3x.server.http;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class ServeFiles implements RequestHandler {
+    private final File root;
+
+    public ServeFiles(File root) {
+        this.root = root;
+    }
+
+    @Override
+    public Response handle(Request request) {
+        File file = new File(root, request.getUri());
+
+        if (!file.exists()) {
+            return new Response(404, "Not Found", new byte[0]);
+        }
+
+        if (file.isDirectory()) {
+            return new Response(200, "OK", getDirectoryContent(file).getBytes());
+        }
+
+        return new Response(200, "OK", read(file));
+    }
+
+    private byte[] read(File file) {
+        try {
+            return Files.readAllBytes(file.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String getDirectoryContent(File directory) {
+        String content = "";
+
+        for (File file : directory.listFiles()) {
+            content += linkTo(file) + "\n";
+        }
+
+        return content;
+    }
+
+    private String linkTo(File file) {
+        String filePath = relativePath(file);
+        return String.format("<a href=\"%s\">%s</a>", filePath, filePath);
+    }
+
+    private String relativePath(File file) {
+        int parentPathLength = root.getAbsolutePath().length();
+        return file.getAbsolutePath().substring(parentPathLength +1);
+    }
+}
---
+++ test/com/github/demonh3x/server/http/ServeFilesTest.java
@@ -0,0 +1,82 @@
+package com.github.demonh3x.server.http;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ServeFilesTest {
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
+
+    ServeFiles serveFiles;
+
+    @Before
+    public void setUp() {
+        serveFiles = new ServeFiles(testFolder.getRoot());
+    }
+
+    @Test
+    public void servesTheContentOfAnExistentTextFile() throws IOException {
+        createFile("dir/file.txt", "content of the file".getBytes());
+
+        Response response = serveFiles.handle(new Request("GET", "dir/file.txt"));
+
+        assertThat(response.getStatusCode(), is(200));
+        assertThat(response.getReasonPhrase(), is("OK"));
+        String body = new String(response.getMessageBody());
+        assertThat(body, is("content of the file"));
+    }
+
+    @Test
+    public void servesTheContentOfAnExistentBinaryFile() throws IOException {
+        createFile("dir/binary-file.bin", new byte[]{0, 1, 2, -128, 127});
+
+        Response response = serveFiles.handle(new Request("GET", "dir/binary-file.bin"));
+
+        assertThat(response.getStatusCode(), is(200));
+        assertThat(response.getReasonPhrase(), is("OK"));
+        assertThat(response.getMessageBody(), is(new byte[]{0, 1, 2, -128, 127}));
+    }
+
+    @Test
+    public void servesTheContentOfAFolder() throws IOException {
+        createFile("dir/file1.txt", "content of the file 1".getBytes());
+        createFile("dir/file2.txt", "content of the file 2".getBytes());
+
+        Response response = serveFiles.handle(new Request("GET", "dir"));
+
+        assertThat(response.getStatusCode(), is(200));
+        assertThat(response.getReasonPhrase(), is("OK"));
+        String body = new String(response.getMessageBody());
+        assertThat(body, containsString("<a href=\"dir/file1.txt\">dir/file1.txt</a>"));
+        assertThat(body, containsString("<a href=\"dir/file2.txt\">dir/file2.txt</a>"));
+        assertThat(body, not(containsString(testFolder.getRoot().getAbsolutePath())));
+    }
+
+    @Test
+    public void servesA404WhenTheFileDoesNotExist() {
+        Response response = serveFiles.handle(new Request("GET", "dir/non-existent-file.txt"));
+
+        assertThat(response.getStatusCode(), is(404));
+        assertThat(response.getReasonPhrase(), is("Not Found"));
+        String body = new String(response.getMessageBody());
+        assertThat(body, is(""));
+    }
+
+    private void createFile(String path, byte[] content) throws IOException {
+        File file = new File(testFolder.getRoot(), path);
+        file.getParentFile().mkdirs();
+        file.createNewFile();
+        Files.write(file.toPath(), content);
+    }
+}
