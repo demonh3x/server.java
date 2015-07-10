facecfade1038fc7456f49e9749b64498adc1c43
Mateu Adsuara <mateuadsuara@gmail.com>
2015-07-10 01:20:43 +0100

    HttpServer is multithreaded

--- src/com/github/demonh3x/server/http/HttpServer.java
+++ src/com/github/demonh3x/server/http/HttpServer.java
@@ -2,13 +2,21 @@ package com.github.demonh3x.server.http;
 
 import com.github.demonh3x.Args;
 import com.github.demonh3x.server.Server;
+import com.github.demonh3x.server.ThreadedHandler;
 
 import java.io.*;
 import java.util.concurrent.Executors;
 
 public class HttpServer extends Server {
     public HttpServer(int port, String root) {
-        super(Executors.newSingleThreadExecutor(), port, new Http(new ServeFiles(new File(root))));
+        super(
+                Executors.newSingleThreadExecutor(),
+                port,
+                new ThreadedHandler(
+                        Executors.newCachedThreadPool(),
+                        new Http(new ServeFiles(new File(root)))
+                )
+        );
     }
 
     public static void main(String[] args) throws IOException {
