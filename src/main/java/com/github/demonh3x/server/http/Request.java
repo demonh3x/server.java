03a4b195a27f39c7571c1d2c74ff36fb06bbb952
Mateu Adsuara <mateuadsuara@gmail.com>
2015-07-14 01:15:02 +0100

    RequestParser reads headers and entity

--- src/main/java/com/github/demonh3x/server/http/Request.java
+++ src/main/java/com/github/demonh3x/server/http/Request.java
@@ -1,12 +1,23 @@
 package com.github.demonh3x.server.http;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Request {
     private final String method;
     private final String uri;
+    private final byte[] messageBody;
+    private final Map<String, String> headers;
 
-    public Request(String method, String uri) {
+    public Request(String method, String uri, byte[] messageBody, Map<String, String> headers) {
         this.method = method;
         this.uri = uri;
+        this.messageBody = messageBody;
+        this.headers = headers;
+    }
+
+    public Request(String method, String uri) {
+        this(method, uri, new byte[0], new HashMap<String, String>());
     }
 
     public String getMethod() {
@@ -15,5 +26,13 @@ public class Request {
 
     public String getUri() {
         return uri;
+    }
+
+    public byte[] getMessageBody() {
+        return messageBody;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
     }
 }
--- src/test/java/com/github/demonh3x/server/http/RequestParserTest.java
+++ src/test/java/com/github/demonh3x/server/http/RequestParserTest.java
@@ -1,32 +1,94 @@
 package com.github.demonh3x.server.http;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class RequestParserTest {
     @Test
-    public void receivesAGetRequestToRootUri() {
+    public void receivesAGetRequestToRootUri() throws IOException {
         Request receivedRequest = readFrom(
-                "GET / HTTP/1.1\n"
+                "GET / HTTP/1.1\r\n" +
+                "\r\n"
         );
         assertThat(receivedRequest.getMethod(), is("GET"));
         assertThat(receivedRequest.getUri(), is("/"));
     }
 
     @Test
-    public void receivesAPostRequestToHomeUri() {
+    public void receivesAPostRequestToHomeUri() throws IOException {
         Request receivedRequest = readFrom(
-                "POST /home HTTP/1.1\n"
+                "POST /home HTTP/1.1\r\n" +
+                "\r\n"
         );
         assertThat(receivedRequest.getMethod(), is("POST"));
         assertThat(receivedRequest.getUri(), is("/home"));
     }
 
-    private Request readFrom(String rawRequest) {
+    @Test
+    public void receivesHeaders() throws IOException {
+        Request receivedRequest = readFrom(
+                "GET / HTTP/1.1\r\n" +
+                "HeaderName1: header:Value1\r\n" +
+                "HeaderName2: header:Value2\r\n" +
+                "\r\n"
+        );
+        assertThat(receivedRequest.getHeaders(), CoreMatchers.<Map<String, String>>is(new HashMap<String, String>() {{
+            put("HeaderName1", "header:Value1");
+            put("HeaderName2", "header:Value2");
+        }}));
+    }
+
+    @Test
+    public void receivesAPostRequestWithAnEntity() throws IOException {
+        Request receivedRequest = readFrom(
+                "POST /home HTTP/1.1\r\n" +
+                "Content-Length: 5\r\n" +
+                "\r\n" +
+                "abcde"
+        );
+        assertThat(receivedRequest.getMessageBody(), is("abcde".getBytes()));
+    }
+
+    @Test
+    public void receivesARequestWithAnEntityButNoContentLength() throws IOException {
+        Request receivedRequest = readFrom(
+                "POST /home HTTP/1.1\r\n" +
+                "\r\n" +
+                "abcde"
+        );
+        assertThat(receivedRequest.getMessageBody(), is(new byte[0]));
+    }
+
+    @Test
+    public void receivesARequestWithContentLengthSmallerThanTheEntity() throws IOException {
+        Request receivedRequest = readFrom(
+                "POST /home HTTP/1.1\r\n" +
+                "Content-Length: 5\r\n" +
+                "\r\n" +
+                "123456789"
+        );
+        assertThat(receivedRequest.getMessageBody(), is("12345".getBytes()));
+    }
+
+    @Test (expected = IOException.class)
+    public void receivesARequestWithContentLengthGreaterThanTheEntity() throws IOException {
+        readFrom(
+                "POST /home HTTP/1.1\r\n" +
+                "Content-Length: 10\r\n" +
+                "\r\n" +
+                "12345"
+        );
+    }
+
+    private Request readFrom(String rawRequest) throws IOException {
         ByteArrayInputStream inputStream = new ByteArrayInputStream(rawRequest.getBytes());
         return new RequestParser().read(inputStream);
     }
